fee1ec565e8824f237f887eba0d5f85a5e0f7424
Ethan <ethanmahc@gmail.com>
2021-03-13 20:10:14 -0700

    Class written for Uploading and Retrieving Mpeg files

--- public/js/record.js
+++ public/js/record.js
@@ -1,52 +1,58 @@
-    // grab microphone from mediaDevices library 
-    navigator.mediaDevices.getUserMedia({audio:true})
-    .then(stream => {handlerFunction(stream)})
+// Front-End File //
+// grab microphone from mediaDevices library
+navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
+  handlerFunction(stream);
+});
 
-    // converting a blob url and sending it to a file
-    // https://stackoverflow.com/questions/60431835/how-to-convert-a-blob-url-to-a-audio-file-and-save-it-to-the-server
-    function handlerFunction(stream) {
-        rec = new MediaRecorder(stream);
-        let blob;
-        let urlMP3;
+// converting a blob url and sending it to a file
+// https://stackoverflow.com/questions/60431835/how-to-convert-a-blob-url-to-a-audio-file-and-save-it-to-the-server
+function handlerFunction(stream) {
+  rec = new MediaRecorder(stream);
+  let blob;
+  let urlMP3;
 
-        rec.ondataavailable = e => {
-        audioChunks.push(e.data);
-        if (rec.state == "inactive"){
-        blob = new Blob(audioChunks,{type:'audio/mpeg-3'});
-        urlMP3 = URL.createObjectURL(blob);
-        recordedAudio.src = URL.createObjectURL(blob);
-        recordedAudio.controls=true;
-        recordedAudio.autoplay=true;
-        sendData(blob)
-        }
+  rec.ondataavailable = (e) => {
+    audioChunks.push(e.data);
+    if (rec.state == "inactive") {
+      blob = new Blob(audioChunks, {type: "audio/mpeg-3"});
+      urlMP3 = URL.createObjectURL(blob);
+      recordedAudio.src = URL.createObjectURL(blob);
+      recordedAudio.controls = true;
+      recordedAudio.autoplay = true;
+      sendData(blob);
     }
+  };
 
-// console.log(blob);
-// console.log(urlMP3);
+  // console.log(blob);
+  // console.log(urlMP3);
 }
 
 function sendData(data) {
-    console.log(data);
-    // write this blob to the google drive folder.
-
-    
+  console.log(data);
+  // send blob to the backend
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/sendBlob", true);
+  // xhr.setRequestHeader('Content-Type', 'application/vnd.google-apps.audio');
+  //xhr.setRequestHeader("Content-Type", "audio/mpeg-3");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(data);
 }
 
 // start recording
-record.onclick = e => {
-    console.log('I was clicked')
-    record.disabled = true;
-    record.style.backgroundColor = "blue"
-    stopRecord.disabled=false;
-    audioChunks = [];
-    rec.start();
-}
+record.onclick = (e) => {
+  console.log("I was clicked");
+  record.disabled = true;
+  record.style.backgroundColor = "blue";
+  stopRecord.disabled = false;
+  audioChunks = [];
+  rec.start();
+};
 
 // stop recording
-stopRecord.onclick = e => {
-    console.log("I was clicked")
-    record.disabled = false;
-    stop.disabled=true;
-    record.style.backgroundColor = "red"
-    rec.stop();
-}
+stopRecord.onclick = (e) => {
+  console.log("I was clicked");
+  record.disabled = false;
+  stop.disabled = true;
+  record.style.backgroundColor = "red";
+  rec.stop();
+};
