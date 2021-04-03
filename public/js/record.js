9a40b0efc87e33cf5a5cd226012b326630353a34
Audrey Chow <audreykchow@gmail.com>
2021-04-02 21:58:10 -0700

    Connected front end to back end
    - Mouse press to add ball, record audio, and send data to Google Drive

--- public/js/record.js
+++ public/js/record.js
@@ -1,3 +1,116 @@
+var person = prompt("Please enter your name", "Harry Potter");
+
+var blue = 'rgb(88, 168, 253)';
+var green = 'rgb(32, 190, 114)';
+var indigo = 'rgb(90, 96, 254)';
+var mustard = 'rgb(248, 188, 72)';
+var orange = 'rgb(248, 84, 48)';
+var plum = 'rgb(143, 23, 97)';
+
+var colors = [blue, green, indigo, mustard, orange, plum];
+
+this.randomColor = colors[Math.floor(Math.random() * colors.length)];
+    
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Common = Matter.Common,
+    Composite = Matter.Composite,
+    Composites = Matter.Composites,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    Events = Matter.Events,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+//Engine (physics)
+var engine = Engine.create(),
+    world = engine.world;
+
+Engine.run(engine);
+
+//Renderer (graphics)
+var render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        wireframes: false,
+        background: 'white'
+    }
+});
+
+Render.run(render);
+
+//Runner (animation)
+var runner = Runner.create();
+Runner.run(runner, engine);
+
+//Mouse Constraint
+var mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,
+            render: {
+                visible: false
+            }
+        }
+    });
+
+World.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+var allBodies = Matter.Composite.allBodies(world);
+var newBall; 
+var foundBall;
+var mouseDownID = 0; 
+var lastSize = 40; //size of most recently added ball
+const maxSize = 300;
+const minSize = 10;
+var listeningID = 0;
+var counter = 0;
+var inflateFactor = 1.01;
+var deflateFactor = 0.999;
+var ground = Bodies.rectangle(window.innerWidth/2, window.innerHeight+15, window.innerWidth, 30, { isStatic: true });
+World.add(world, [ground]);
+
+//Update message, ball size
+Events.on(engine, 'beforeUpdate', function(event) {
+    
+    lastSize *= inflateFactor;
+    
+    if (mouseDownID == 1 && lastSize < maxSize && !foundBall)    {
+        Matter.Body.scale(newBall, inflateFactor, inflateFactor);
+        document.getElementById("message").innerHTML = "Recording...";
+    } else if (listeningID == 1){
+        counter += 1;
+        Matter.Body.scale(foundBall, deflateFactor, deflateFactor);
+        
+        document.getElementById("message").innerHTML = "Listening...";
+    } else if (listeningID == 0) {
+        counter = 0;
+        
+        if (person != null) document.getElementById("message").innerHTML = "Good morning, " + person;
+    }
+});
+
+//Check hover on balls
+Matter.Events.on(mouseConstraint, 'mousemove', function(event) {
+    var bodies = Matter.Composite.allBodies(world);
+    var foundPhysics = Matter.Query.point(bodies, event.mouse.position);
+    foundBall = foundPhysics[0];
+    
+    if (foundBall && mouseDownID == 1) {
+        listeningID = 1;
+    } else {
+        listeningID = 0;
+    }
+});
+
 // Front-End File //
 // grab microphone from mediaDevices library
 navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
@@ -10,30 +123,55 @@ function handlerFunction(stream) {
   rec = new MediaRecorder(stream);
   let blob;
   let urlMP3;
+    
+    Events.on(mouseConstraint, 'mousedown', function(event) {
+        
+        mouseDownID = 1;
 
-  // send stream of data on start click
-  document.getElementById("record").addEventListener("click", function () {
-    rec.ondataavailable = (e) => {
-      audioChunks.push(e.data);
-      if (rec.state == "inactive") {
-        blob = new Blob(audioChunks, {type: "audio/mpeg-3"});
-        recordedAudio.src = URL.createObjectURL(blob);
-        recordedAudio.controls = true;
-        recordedAudio.autoplay = true;
+        //If mouse is pressed and not over a ball, add a new ball and record audio
+        if (!foundBall) {
 
-        // var myObj = {
-        //   blob: blob, // type: audio/mpeg-3
-        //   // name: "Ethan", // pass in file-name here?
-        // };
+            //record audio
+            record.disabled = true;
+            record.style.backgroundColor = "blue";
+            stopRecord.disabled = false;
+            audioChunks = [];
+            rec.start();
 
-        // console.log(blob);
-        // stringify the blob
-        // sendData(JSON.stringify(myObj));
-        sendData(blob);
-      }
-    };
-  });
+            //send data
+            rec.ondataavailable = (e) => {
+                audioChunks.push(e.data);
+
+                if (rec.state == "inactive") {
+                    blob = new Blob(audioChunks, {type: "audio/mpeg-3"});
+                    recordedAudio.src = URL.createObjectURL(blob);
+                    recordedAudio.controls = true;
+                    recordedAudio.autoplay = true;
+                    sendData(blob);
+                }
+            };
+
+            //create ball
+            newBall = Bodies.circle(event.mouse.position.x, event.mouse.position.y, 40, {
+                render: {
+                    fillStyle: colors[Math.floor(Math.random() * colors.length)]
+                }
+            });
+            World.add(world, newBall);
+        }
+    });
 }
+
+Events.on(mouseConstraint, 'mouseup', function(event) {
+    mouseDownID = 0;
+    lastSize = 40;
+    
+    //stop recording
+    record.disabled = false;
+    stop.disabled = true;
+    record.style.backgroundColor = "red";
+    rec.stop();
+});
 
 function sendData(data) {
   // console.log(data);
@@ -70,22 +208,3 @@ function getData(id) {
   };
   request.send();
 }
-
-// start recording
-record.onclick = (e) => {
-  console.log("I was clicked");
-  record.disabled = true;
-  record.style.backgroundColor = "blue";
-  stopRecord.disabled = false;
-  audioChunks = [];
-  rec.start();
-};
-
-// stop recording
-stopRecord.onclick = (e) => {
-  console.log("I was clicked");
-  record.disabled = false;
-  stop.disabled = true;
-  record.style.backgroundColor = "red";
-  rec.stop();
-};
